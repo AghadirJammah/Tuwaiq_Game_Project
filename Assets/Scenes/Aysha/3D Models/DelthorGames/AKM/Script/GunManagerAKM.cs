9fe41759ac64f2dfecc90ef60035e896e8e148f9
AyshaHamad <ayshtaltwyjry@gmail.com>
2025-07-24 18:29:07 +0300

    Environment

---
+++ Assets/Scenes/Aysha/3D Models/DelthorGames/AKM/Script/GunManagerAKM.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GunManagerAKM : MonoBehaviour
+{
+    public GameObject BulletPrefap;
+    public Transform FirePoint;
+    public float BulletSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject bullet = null;
+            bullet = Instantiate(BulletPrefap, FirePoint.position, Quaternion.identity);
+
+            Rigidbody bulletPhyx = null;
+            bulletPhyx = bullet.GetComponent<Rigidbody>();
+
+            bulletPhyx.AddForce(FirePoint.forward * BulletSpeed, ForceMode.Impulse);
+
+        }
+    }
+}
