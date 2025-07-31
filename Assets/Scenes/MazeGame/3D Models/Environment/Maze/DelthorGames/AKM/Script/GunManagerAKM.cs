4d081e325acd7c8748f0f87315a76fd43cc1cc07
AghadirJammah <aghadirjammah@gmail.com>
2025-07-31 03:39:10 +0300

    Updates

---
+++ Assets/Scenes/MazeGame/3D Models/Environment/Maze/DelthorGames/AKM/Script/GunManagerAKM.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GunManagerAKM : MonoBehaviour
+{
+    public GameObject BulletPrefap;         
+    public GameObject muzzleFlashPrefab;    
+    public Transform FirePoint;             
+    public float BulletSpeed = 20f;          
+    public AudioSource audioSource;         
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+
+            
+            GameObject bullet = Instantiate(BulletPrefap, FirePoint.position, FirePoint.rotation);
+            Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+            if (bulletRb != null)
+            {
+                bulletRb.AddForce(FirePoint.forward * BulletSpeed, ForceMode.Impulse);
+            }
+
+            
+            if (muzzleFlashPrefab != null)
+            {
+                GameObject flash = Instantiate(muzzleFlashPrefab, FirePoint.position, FirePoint.rotation);
+                ParticleSystem ps = flash.GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Play();
+                }
+                Destroy(flash, 0.1f); 
+            }
+        }
+    }
+}
+
+
